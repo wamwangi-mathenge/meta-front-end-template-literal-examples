1867a59932c050f1eecc684008849b0f2e92af1b
Brian <wamwangimathenge@gmail.com>
2023-08-14 14:39:03 +0300

    Rewrite the first example using the + operator
    This commit demonstrates how the first code snippet would have been
    written in ES5

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Demonstrate how template literals allow for variable interpolation
 let greet = "Hello";
 let place = "World";
-console.log(`${greet} ${place} !`)
+console.log(`${greet} ${place}!`)
+
+// Rewrite the above example using the + operator
+var greet1 = "Hello";
+var place1 = "World";
+console.log(greet1 + " " + place + "!" )
